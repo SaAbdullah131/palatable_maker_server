4e29a9d1990da33dfb871b7ede8f131c346e7590
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-04 20:52:17 +0600

    vercel json added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app= express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+// cors middleware form express
+app.use(cors());
+
 app.get('/',(req,res)=> {
-    res.send('chef-recipe-server running');
+    res.send('palatableMaker-server running');
 })
+
+
+// app listen port number 5000
 app.listen(port,()=> {
     console.log(`check-recipe-server api is running ${port}`);
 })
