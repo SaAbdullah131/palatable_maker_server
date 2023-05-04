51f8fe55bc886ad6d94fc7f480f5b3f7f4712558
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-04 21:03:41 +0600

    data added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app= express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+//load data
+const chef = require('./data/data.json');
 
 // cors middleware form express
 app.use(cors());
@@ -9,7 +11,6 @@ app.use(cors());
 app.get('/',(req,res)=> {
     res.send('palatableMaker-server running');
 })
-
 
 // app listen port number 5000
 app.listen(port,()=> {
