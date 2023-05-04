bea17f348be431968f60de9b419fb81616e13b99
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-04 21:15:07 +0600

    all chefs data routes setup done

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app= express();
-const cors = require('cors');
+// const cors = require('cors');
 const port = process.env.PORT || 5000;
-//load data
-const chef = require('./data/data.json');
+// load Data
+const chefs = require('./data/data.json');
 
 // cors middleware form express
-app.use(cors());
+//app.use(cors());
 
 app.get('/',(req,res)=> {
     res.send('palatableMaker-server running');
 })
+// all chefs and recipes 
+app.get('/chefs', (req, res)=>{
+    res.send(chefs);
+})
+
+// 
 
 // app listen port number 5000
 app.listen(port,()=> {
